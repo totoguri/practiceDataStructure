89f309c293c400488c3916325c53cf86e12b90c6
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-21 18:37:49 +0900

    data

---
+++ practiceDataStructureWithC/Source.c
@@ -0,0 +1,36 @@
+// Data Structure ¿­Ç÷ °­ÀÇ
+// Start at 21, Oct, 2021
+
+#include <stdio.h>
+
+//int main(int argc, char** argv) {
+//	int i;
+//
+//	puts("Now I will count from 1 to 10");
+//	for (i = 1; i <= 10; i++) {
+//		printf("%d\n", i);
+//	}
+//
+//	return 0;
+//}
+
+
+//#include <stdlib.h>
+//#include <assert.h>
+//
+//int main(int argc, char** argv) {
+//	char* s;
+//	s = malloc(2);
+//	free(s);
+//
+//	s[0] = 'a';
+//	s[1] = '\0';
+//
+//	puts(s);
+//	return 0;
+//}
+
+
+// p.77
+
+
