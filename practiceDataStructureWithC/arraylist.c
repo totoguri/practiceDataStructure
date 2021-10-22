c6d4732cdf99b1515821c47866474f7bbaa305f9
DESKTOP-RV7V0F9\totog <finalyo4@naver.com>
2021-10-22 18:34:16 +0900

    data

--- practiceDataStructureWithC/arraylist.c
+++ practiceDataStructureWithC/arraylist.c
@@ -84,3 +84,124 @@ int removeALElement(ArrayList* pList, int position) {
 	return ret;
 }
 
+ArrayListNode* getALElement(ArrayList* pList, int position) {
+	ArrayListNode* pReturn = NULL;
+	if (pList != NULL) {
+		if (position >= 0 && position < getArrayListLength(pList)) {
+			pReturn = &(pList->pElement[position]);
+		}
+		else {
+			printf("Error, out of index-[%d] range, getALElement() \n", position);
+		}
+	}
+
+	return pReturn;
+}
+
+void displayArrayList(ArrayList* pList) {
+	int i = 0;
+	if (pList != NULL) {
+		printf("Maxinum numbers: %d\n", pList->maxElementCount);
+		printf("Current numbers: %d\n", pList->currentElementCount);
+
+		for (i = 0; i < pList->currentElementCount; i++) {
+			printf("[%d], %d\n", i, getALElement(pList, i)->data);
+		}
+
+		i = pList->currentElementCount;
+		for (; i < pList->maxElementCount; i++) {
+			printf("[%d], Empty\n", i);
+		}
+	}
+	else {
+		printf("ArrayList is NULL");
+	}
+}
+
+int isArrayListFull(ArrayList* pList) {
+	int ret = FALSE;
+
+	if (pList != NULL) {
+		if (pList->currentElementCount == pList->maxElementCount) {
+			ret = TRUE;
+		}
+	}
+
+	return ret;
+}
+
+int getArrayListLength(ArrayList* pList) {
+	int ret = 0;
+
+	if (pList != NULL) {
+		ret = pList->currentElementCount;
+	}
+
+	return ret;
+}
+
+void deleteArrayList(ArrayList* pList) {
+	int i = 0;
+	if (pList != NULL) {
+		free(pList->pElement);
+		free(pList);
+	}
+}
+
+int addALElementFirst(ArrayList* pList, ArrayListNode element) {
+	int position = 0;
+	return addALElement(pList, position, element);
+}
+
+int addALElementLast(ArrayList* pList, ArrayListNode element) {
+	int ret = FALSE;
+	int position = 0;
+	if (pList != NULL) {
+		position = getArrayListLength(pList);
+		ret = addALElement(pList, position, element);
+	}
+
+	return ret;
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "arraylist.h"
+
+int main(int argc, char* argv[]) {
+	int i = 0;
+	int arrayCount = 0;
+	ArrayList* pList = NULL;
+	ArrayListNode* pValue = NULL;
+
+	pList = createArrayList(6);
+
+	if (pList != NULL) {
+		ArrayListNode node;
+		node.data = 1;
+		addALElement(pList, 0, node);
+
+		node.data = 3;
+		addALElement(pList, 1, node);
+
+		node.data = 5;
+		addALElement(pList, 2, node);
+		displayArrayList(pList);
+
+		removeALElement(pList, 0);
+		node.data = 7;
+		addALElementFirst(pList, node);
+		displayArrayList(pList);
+
+		arrayCount = getArrayListLength(pList);
+		for (i = 0; i < arrayCount; i++) {
+			pValue = getALElement(pList, i);
+			printf("Position[%d] = %d\n", i, pValue->data);
+		}
+		deleteArrayList(pList);
+	}
+
+	return 0;
+}
+
+// p.138
